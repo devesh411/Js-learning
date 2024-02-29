593fe5b73f713c5c55b5bdf9094a459c9ecb2d2f
devesh411 <141461448+devesh411@users.noreply.github.com>
2024-02-29 21:31:17 +0000

    Operations

--- .devcontainer/01_basics/conversionOperations.js
+++ .devcontainer/01_basics/conversionOperations.js
@@ -23,5 +23,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//**********************Operations******************/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+let str = "hello"
+let str2 = "Devesh"
+let str3 = str + str2
+// console.log(str3);
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3 = 2
+console.log(num2);
+
+let gameCounter = 100
+++gameCounter
+// console.log(gameCounter);
+
+let x = 3 
+let y = x++
+
+let x2 = 3
+let y2 = ++x2
+
+// console.log(y);
+// console.log(y2);
