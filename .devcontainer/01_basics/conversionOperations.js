091a9b0e636f8f94c16c7795cf927cd135ece1bf
devesh411 <141461448+devesh411@users.noreply.github.com>
2024-02-29 21:08:10 +0000

    Conversion of data types

---
+++ .devcontainer/01_basics/conversionOperations.js
@@ -0,0 +1,27 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueIntNumber = Number(score)
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);
+
+//"22"=> 33
+// "33abc" = Nan
+// true => 1; false =>0
+
+let isLoggedIn = 2
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "devesh" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
